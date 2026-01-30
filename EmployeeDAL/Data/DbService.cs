277fdcf11537e0b936816b60babad48c92a5288f
Trupal Kaila <trupalkaila8@gmail.com>
2026-01-30 14:52:31 +0530

    Add project files.

---
+++ EmployeeDAL/Data/DbService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDAL.Data
+{
+    public sealed class DbService
+    {
+        private static readonly Lazy<DbService> _instance = new Lazy<DbService>(() => new DbService());
+        private readonly string _connectionString;
+
+        private DbService()
+        {
+            _connectionString = "Server=(LocalDb)\\MSSQLLocalDb;Database=Practical22;Trusted_Connection=True;";
+        }
+
+        public static DbService Instance => _instance.Value;
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
